0890176416fa228bac2dde9e79fa56f08edc02d0
Jarek <m4n0fsh4dow@gmail.com>
2022-03-04 15:54:59 -0500

    - p

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
-#include <iostream>
-#include "Punkt.cpp"
-using namespace std;
+#include <ncurses.h>
 
 int main()
-{
-    Punkt pkt = Punkt(1, 2);
-    pkt.wyswietl();
-    return 0;
+{	
+	initscr();			            
+	printw("Hello World !!!");	    
+	refresh();			            
+	getch();			            
+	endwin();			            
+	return 0;
 }
+
+// #include <iostream>
+// #include "Punkt.cpp"
+// using namespace std;
+
+// int main()
+// {
+//     Punkt pkt = Punkt(1, 2);
+//     pkt.wyswietl();
+//     return 0;
+// }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: main
 
 main: main.o
-	g++ -o main main.o
+	g++ -o main main.o -lncurses
 
 main.o: main.cpp
-	g++ -c main.cpp
+	g++ -c main.cpp 
